45c8c5a55a7d6f27b090183deb941a9b19859c5a
tera <misaret@gmail.com>
2014-05-11 00:52:10 +0300

    added Html helper

---
+++ Html.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace misaret\utils;
+
+class Html
+{
+	static public function linkVars($skeep = null, $input = null)
+	{
+		if ($input === null) {
+			$input = $_GET;
+		}
+		if ($skeep) {
+			if (is_array($skeep)) {
+				foreach ($skeep as $val) {
+					unset($input[$val]);
+				}
+			} else {
+				unset($input[$skeep]);
+			}
+		}
+		if (!$input) {
+			return '';
+		}
+
+		return implode('&', array_map(function ($key, $val) {
+			return urlencode($key) . '=' . urlencode($val);
+		}, array_keys($input), array_values($input)));
+	}
+}
