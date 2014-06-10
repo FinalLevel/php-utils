0c9e8736cdc1bd87e54978205128b248cd5ed17e
tera <misaret@gmail.com>
2014-06-10 13:00:07 +0300

    added Util::mail()

--- Utils.php
+++ Utils.php
@@ -4,6 +4,13 @@ namespace fl\utils;
 
 class Utils
 {
+	/**
+	 * Save $content to file
+	 *
+	 * @param string $file
+	 * @param string $content
+	 * @return boolean
+	 */
 	static public function saveFile($file, $content)
 	{
 		$res = file_put_contents($file, $content);
@@ -17,6 +24,14 @@ class Utils
 		return true;
 	}
 
+	/**
+	 * Save $data file as php.
+	 * Then use $data = include($file)
+	 *
+	 * @param string $file
+	 * @param mixed $data
+	 * @return boolean
+	 */
 	static public function saveStatic($file, $data)
 	{
 		$str = var_export($data, true);
@@ -25,10 +40,56 @@ class Utils
 		return static::saveFile($file, $content);
 	}
 
+	/**
+	 * Save $data file as JSON.
+	 *
+	 * @param string $file
+	 * @param mixed $data
+	 * @return boolean
+	 */
 	static public function saveJson($file, $data)
 	{
 		$content = json_encode($data, JSON_UNESCAPED_UNICODE);
 
 		return static::saveFile($file, $content);
+	}
+
+	/**
+	 * Send simple mail with right headers formating
+	 *
+	 * @param array $from
+	 * @param array $to
+	 * @param string $subject
+	 * @param string $body
+	 */
+	static function mail($from, $to, $subject, $body)
+	{
+		// Create the Transport
+		$transport = \Swift_MailTransport::newInstance();
+
+		// Create the Mailer using your created Transport
+		$mailer = \Swift_Mailer::newInstance($transport);
+
+		// Create a message
+		$message = \Swift_Message::newInstance()
+			->setFrom($from)
+			->setTo($to)
+			->setSubject($subject)
+			->setBody($body);
+
+		// Send the message
+		$result = $mailer->send($message);
+
+		$toStr = [];
+		foreach ((array) $to as $key => $val) {
+			if (is_int($key)) {
+				$toStr[] = $val;
+			} else {
+				$toStr[] = "$val <$key>";
+			}
+		}
+		trigger_error('Mail sent to ' . implode(', ', $toStr), ($result ? E_USER_NOTICE : E_USER_WARNING));
+
+		return $result;
 	}
 }
